a6a357995bc79f6b9dc2aa22f39de6679aaf89ec
m-vargas-h <m.vargas@outlook.cl>
2025-08-25 13:59:12 -0400

    Carga proyecto base

---
+++ Exp1_S2/src/main/java/com/duoc/exp1_s2/app/Exp1_S2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.duoc.exp1_s2.app;
+
+/**
+ *
+ * @author mvarg
+ */
+public class Exp1_S2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
